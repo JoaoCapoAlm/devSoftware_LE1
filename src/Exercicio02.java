01619993d172d4be94b6bf9e02f78b1369256896
João Pedro <jpcapoani98@gmail.com>
2024-03-14 17:26:42 -0300

    Resolucao exercicio 02

---
+++ src/Exercicio02.java
@@ -0,0 +1,16 @@
+public class Exercicio02 {
+    public static void Executar(){
+        Prompt.imprimir("Exercício 2");
+        Prompt.imprimir("Escreva um algoritmo que leia dois números digitados pelo usuário e exiba o " +
+                "resultado da sua soma. ");
+        Prompt.linhaEmBranco();
+
+        System.out.print("Digite o primeiro valor: ");
+        var num1 = Prompt.lerDecimal();
+
+        System.out.print("Digite o segundo valor: ");
+        var num2 = Prompt.lerDecimal();
+
+        Prompt.imprimir("Soma: " + (num1 + num2));
+    }
+}
