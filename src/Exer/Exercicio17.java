ce9c2ac48bc9087b39ed0c934c63dc189165537a
João Pedro <jpcapoani98@gmail.com>
2024-03-17 21:29:31 -0300

    Exercício 17 - ajuste enunciado

--- src/Exer/Exercicio17.java
+++ src/Exer/Exercicio17.java
@@ -9,6 +9,8 @@ public class Exercicio17 {
                 "cada nota e retorne a sua média ponderada.", true);
         Prompt.imprimir("Veja o cálculo da média ponderada:", true);
         Prompt.imprimir("mp = (nota1 * peso1 + nota2 * peso2 + nota3 * peso3) / (peso1 + peso2 + peso3)", true);
+        Prompt.imprimir("Exemplo: nota1 = 10, nota2 = 5.5, nota3 = 8, peso1 = 5, peso2 = 3, peso3 = 2", true);
+        Prompt.imprimir("Média ponderada = 8.25");
         Prompt.linhaEmBranco();
 
         Prompt.imprimir("Insira os valores a baixo", true);
